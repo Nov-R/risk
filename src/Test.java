7b5ffe5470f697bae9433d53b97373dc63390406
Nov-R <116569539@qq.com>
2025-10-20 17:54:50 +0800

    11

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        long l = 200l;
+        int i = (int)l;
+        System.out.println("Long value: " + l);
+        System.out.println("Int value: " + i);
+    }
+}
